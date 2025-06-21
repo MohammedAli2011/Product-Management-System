98b319f8c84943693170e9b626094ef576eb8fdd
MohammedAli2011 <engineer.mohammed.ali11@gmail.com>
2025-06-22 01:18:54 +0300

    after keep data in local storege

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ let price = document.getElementById("price");
 let taxes = document.getElementById("taxes");
 let ads = document.getElementById("ads");
 let discount = document.getElementById("discount");
+let totalC = document.getElementById("totalC");
 let total = document.getElementById("total");
 let count = document.getElementById("count");
 let category = document.getElementById("category");
@@ -13,10 +14,34 @@ let btn = document.getElementById("btn");
 function getTotal() {
     if(price.value != "") {
         let result = (+price.value + +taxes.value + +ads.value) - +discount.value;
-        total.style.background = "#198754"
-        total.innerHTML = `Total : ${result}`
+        totalC.classList.remove("bg-danger");
+        totalC.classList.add("bg-success");
+        total.innerHTML = result;
     }else {
-        total.style.background = "#dc3545"
-        total.innerHTML = "Total : "
-    }
+        totalC.style.background = "#dc3545";
+        total.innerHTML = "";
+    };
 };
+// create products
+let pro;
+if(localStorage.pro != null) {
+    pro = JSON.parse(localStorage.pro);
+}else{
+    pro=[];
+};
+btn.onclick = function() {
+    let products ={
+        title:title.value,
+        price:price.value,
+        taxes:taxes.value,
+        ads:ads.value,
+        discount:discount.value,
+        total:total.innerHTML,
+        count:count.value,
+        category:category.value,
+    };
+    pro.push(products);
+    localStorage.setItem("pro", JSON.stringify(pro) );
+    console.log(pro);
+};
+// clear inputs
